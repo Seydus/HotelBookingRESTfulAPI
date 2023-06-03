ac3a7c10e01608196542c4332493449df1f52a0c
Weljohn Catanpatan <weljohncatanpatan@gmail.com>
2023-06-03 22:42:32 +0800

    Upload

---
+++ HotelBookingRESTfulAPI/Data/APIContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using HotelBookingAPI.Models;
+
+namespace HotelBookingAPI.Data
+{
+    public class APIContext : DbContext
+    {
+        public DbSet<HotelBooking> Bookings { get; set; }
+
+        public APIContext(DbContextOptions<APIContext> options) 
+            : base(options) { }
+
+
+    }
+}
